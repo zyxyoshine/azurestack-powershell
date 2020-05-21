7d320efcec7bd507375351e869cb67bf7998c426
Vishnu Priya Ananthu Sundaram <viananth@microsoft.com>
2020-05-21 16:49:43 -0700

    Remove debug messages

--- src/Azs.KeyVault.Admin/test/Get-AzsKeyvaultQuota.Tests.ps1
+++ src/Azs.KeyVault.Admin/test/Get-AzsKeyvaultQuota.Tests.ps1
@@ -1,7 +1,6 @@
 # Load Default parameter values
 
 $envFile = 'env.json'
-Write-Host "Loading env.json"
 if ($TestMode -eq 'live') {
     $envFile = 'localEnv.json'
 }
@@ -14,15 +13,10 @@ if (Test-Path -Path (Join-Path $PSScriptRoot $envFile)) {
 $env = @{}
 if (Test-Path -Path $envFilePath) {
     $env = Get-Content (Join-Path $PSScriptRoot $envFile) | ConvertFrom-Json
-    Write-Host "******In loadenv.ps1******"
-    Write-Host $env.Values
     $PSDefaultParameterValues=@{"*:SubscriptionId"=$env.SubscriptionId; "*:Tenant"=$env.Tenant}
     $PSDefaultParameterValues.Add("*:Location", $env.Location)
-    Write-Host "Default values: $($PSDefaultParameterValues.Values)"
 }
 
-Write-Host "Default parameter values"
-Write-Host "Default values: $($PSDefaultParameterValues.Values)"
 $TestRecordingFile = Join-Path $PSScriptRoot 'Get-AzsKeyvaultQuota.Recording.json'
 $currentPath = $PSScriptRoot
 while(-not $mockingPath) {

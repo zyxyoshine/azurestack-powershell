5767672844143e2a0d10f7121a57473f0c03af05
Wei Shi <weishi9999@gmail.com>
2020-06-19 18:38:05 -0700

    update test

--- src/Azs.Update.Admin/test/UpdateAdmin.Tests.ps1
+++ src/Azs.Update.Admin/test/UpdateAdmin.Tests.ps1
@@ -51,24 +51,15 @@ $global:TestName = ""
 $global:Provider = "Microsoft.Update.Admin"
 $global:ModuleName = "Azs.Update.Admin"
 
+$TestRecordingFile = Join-Path $PSScriptRoot 'UpdateAdmin.Recording.json'
+$currentPath = $PSScriptRoot
+while(-not $mockingPath) {
+    $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
+    $currentPath = Split-Path -Path $currentPath -Parent
+}
+. ($mockingPath | Select-Object -First 1).FullName
+
 InModuleScope Azs.Update.Admin {
-
-    $loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
-    if (-Not (Test-Path -Path $loadEnvPath)) {
-        $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
-    }
-    . ($loadEnvPath)
-    $TestRecordingFile = Join-Path $PSScriptRoot 'UpdateAdmin.Recording.json'
-    $currentPath = $PSScriptRoot
-    while(-not $mockingPath) {
-        $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
-        $currentPath = Split-Path -Path $currentPath -Parent
-    }
-    . ($mockingPath | Select-Object -First 1).FullName
-
-    Import-Module ..\$global:ModuleName -Force
-    Import-Module Az.Resources -Force 
-
     Describe "UpdateAdminTests" -Tags @('UpdateAdminTests', 'Azs.Update.Admin') {
 
         BeforeEach {

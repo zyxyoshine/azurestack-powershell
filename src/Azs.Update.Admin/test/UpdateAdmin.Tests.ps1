74ecbb1dfd0bf914df1de1e36b47dac624fbc7aa
Wei Shi <weishi9999@gmail.com>
2020-06-29 16:49:38 -0700

    update test

--- src/Azs.Update.Admin/test/UpdateAdmin.Tests.ps1
+++ src/Azs.Update.Admin/test/UpdateAdmin.Tests.ps1
@@ -51,23 +51,6 @@ $global:ModuleName = "Azs.Update.Admin"
 . (Join-Path $PSScriptRoot 'loadEnvJson.ps1')
 
 InModuleScope Azs.Update.Admin {
-
-    $loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
-    if (-Not (Test-Path -Path $loadEnvPath)) {
-        $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
-    }
-    . ($loadEnvPath)
-    $TestRecordingFile = Join-Path $PSScriptRoot 'UpdateAdmin.Recording.json'
-    $currentPath = $PSScriptRoot
-    while(-not $mockingPath) {
-        $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
-        $currentPath = Split-Path -Path $currentPath -Parent
-    }
-    . ($mockingPath | Select-Object -First 1).FullName
-
-    Import-Module ..\$global:ModuleName -Force
-    Import-Module Az.Resources -Force 
-
     Describe "UpdateAdminTests" -Tags @('UpdateAdminTests', 'Azs.Update.Admin') {
 
         BeforeEach {
